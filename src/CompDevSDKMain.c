0cbc57321af2d51305aa3aa1bd8094482889cebb
Sebastian Zientek <sebastian.zientek1@gmail.com>
2020-10-28 08:01:05 +0100

    WIP

--- src/CompDevSDKMain.c
+++ src/CompDevSDKMain.c
@@ -16,14 +16,29 @@ TaskHandle_t BlinkTaskHandle;
 TaskHandle_t RadioTaskHandle;
 TaskHandle_t ClockTaskHandle;
 TaskHandle_t SystemInitTaskHandle;
+TaskHandle_t MapTaskHandle;
 
 ds1703_time_t time;
+
+
+char gameMap[2][21];
+char displayBuffer[2][21];
+
+typedef struct{
+    uint8_t x, y;
+} Player;
+Player player = {0, 0};
 
 enum {
     CLOCK,
     MENU,
     SETTING_CLOCK
 } mode;
+
+enum {
+    ONGOING,
+    GAMEOVER
+} gameState = ONGOING;
 
 void transmitterTask(void *param)
 {
@@ -90,15 +105,25 @@ void receiverTask(void *param)
             uint8_t buffer[10];
             nrf24ReadData(buffer, 10, NULL);
             displaySetPos(0, 0);
-            displayPrintf("R: %s     ", buffer);
-            uartPrintf("R: %s\r\n", buffer);
+            // displayPrintf("R: %s     ", buffer);
+            // uartPrintf("R: %s\r\n", buffer);
+
+            if (strncmp("DOWN", buffer, 4) == 0)
+            {
+                player.y = 1;
+            }
+            else if (strncmp("UP", buffer, 4) == 0)
+            {
+                player.y = 0;
+            }
         }
 
         displaySetPos(0, 1);
         int val = analogRead(A0);
         if (val > 110 && val < 120)
         {
-            displayPrint("UP    ");
+            // displayPrint("UP    ");
+            player.y = 0;
         }
         if(val > 180 && val < 190)
         {
@@ -110,7 +135,8 @@ void receiverTask(void *param)
         }
         else if(val > 1000 && val < 1050)
         {
-            displayPrint("DOWN  ");
+            // displayPrint("DOWN  ");
+            player.y = 1;
         }
         else if(val > 270 && val < 280)
         {
@@ -121,15 +147,108 @@ void receiverTask(void *param)
     }
 }
 
+void mapTask(void *param)
+{
+    (void)param;
+
+
+    while (1)
+    {
+        if (gameState == ONGOING)
+        {
+            memcpy(gameMap[0], gameMap[0] + 1, 19);
+            memcpy(gameMap[1], gameMap[1] + 1, 19);
+
+            if (gameMap[1][19] == ' ' && gameMap[1][18] == ' ')
+            {
+                gameMap[0][19] = rand() % 5 ? ' ' : 0x14;
+            }
+            else
+            {
+                gameMap[0][19] = ' ';
+            }
+
+            if (gameMap[0][19] == ' ' && gameMap[0][18] == ' ')
+            {
+                gameMap[1][19] = rand() % 5 ? ' ' : 0x14;
+            }
+            else
+            {
+                gameMap[1][19] = ' ';
+            }
+        }
+
+
+        vTaskDelay(100/portTICK_PERIOD_MS);
+    }
+}
+
+
+// void drawMap()
+// {
+//     displaySetPos(0, 0);
+//     displayPrint(gameMap[0]);
+//     displaySetPos(0, 1);
+//     displayPrint(gameMap[1]);
+// }
+
+void drawDisplayBuffer()
+{
+    displaySetPos(0, 0);
+    displayPrint(displayBuffer[0]);
+    displaySetPos(0, 1);
+    displayPrint(displayBuffer[1]);
+}
+
+void clearDisplayBuffer()
+{
+    for (int i = 0; i < 20; ++i)
+    {
+        displayBuffer[0][i] = ' ';
+        displayBuffer[1][i] = ' ';
+    }
+    displayBuffer[0][20] = '\0';
+    displayBuffer[1][20] = '\0';
+}
+
+void initialGenerateMap()
+{
+    for (int i = 0; i < 20; ++i)
+    {
+        gameMap[0][i] = ' ';
+        gameMap[1][i] = ' ';
+    }
+    gameMap[0][20] = '\0';
+    gameMap[1][20] = '\0';
+}
+
 void blinkTask(void *param)
 {
     (void)param;
+
+    clearDisplayBuffer();
+    initialGenerateMap();
+
     while (1)
     {
-        // setPinValue(5, !getPinValue(5));
-        char c[] = {0x1D, 0x00};
-        displayPrint(c);
-        vTaskDelay(1000/portTICK_PERIOD_MS);
+        switch (gameState)
+        {
+            case ONGOING:
+                memcpy(displayBuffer, gameMap, sizeof(gameMap));
+
+                if (gameMap[player.y][player.x] != ' ') gameState = GAMEOVER;
+
+                displayBuffer[player.y][player.x] = 0x1D;
+                drawDisplayBuffer();
+                clearDisplayBuffer();
+            break;
+            case GAMEOVER:
+                displaySetPos(5, 0);
+                displayPrint("GAME  OVER");
+            break;
+        }
+
+        vTaskDelay(50/portTICK_PERIOD_MS);
     }
 }
 
@@ -206,6 +325,7 @@ void systemInitTask(void *param)
     #endif
     #ifdef RECEIVER
         xTaskCreate(blinkTask, "BlinkTask", 140, NULL, 1, &BlinkTaskHandle);
+        xTaskCreate(mapTask, "MapTask", 100, NULL, 1, &MapTaskHandle);
         xTaskCreate(receiverTask, "ReceiverRadioTask", 200, NULL, 1, &RadioTaskHandle);
     #endif
 
